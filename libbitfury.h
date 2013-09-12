b65b972f57d45c4c56103e606c9203fd1b566836
alpet83 <alpet1@gmail.com>
2013-09-12 01:36:07 -0700

    added fields to bitfury_devices, added typedef PBITFURY_DEVICE and PTIMEVAL
    New fields: osc6_bits_upd, work_start, work_end, work_median, work_wait

--- libbitfury.h
+++ libbitfury.h
@@ -31,11 +31,11 @@
 
 #define BITFURY_STAT_N 1024
 #define BITFURY_MAXCHIPS 256
-#define BITFURY_MAXBANKS 1
-#define BITFURY_BANKCHIPS 60
+#define BITFURY_MAXBANKS 16
+#define BITFURY_BANKCHIPS 8
 
-//#define BITFURY_ENABLE_SHORT_STAT 1
-//#define BITFURY_ENABLE_LONG_STAT 1
+#define BITFURY_ENABLE_SHORT_STAT 1
+#define BITFURY_ENABLE_LONG_STAT 1
 
 #define BITFURY_API_STATS 300
 
@@ -50,12 +50,17 @@ struct bitfury_payload {
 
 struct bitfury_device {
 	unsigned osc6_bits;
-	unsigned osc6_bits_setpoint;
+    unsigned osc6_bits_setpoint;
+    unsigned osc6_bits_upd;
 	unsigned newbuf[17];
 	unsigned oldbuf[17];
 	struct work * work;
 	struct work * owork;
 	struct work * o2work;
+    struct timeval work_start;
+    struct timeval work_end;
+    double work_median;  // median time in microseconds
+    double work_wait;    // средний простой в ожидании работы
 	int job_switched;
 	struct bitfury_payload payload;
 	struct bitfury_payload opayload;
@@ -83,11 +88,17 @@ struct bitfury_device {
 	unsigned slot;
 	unsigned fasync;
 	unsigned hw_errors;
+    int      alerts;
+    float    hw_rate;
+    bool     fixed_clk;
+    float    rbc_stat[4];   // rate-by-clock: 53, 54, 55, 56 associated to chip hash-rate
 	unsigned int matching_work;
 	unsigned int nonces[32];
 	int current_nonce;
-	double gh_stat[8];
 };
+
+typedef struct bitfury_device *PBITFURY_DEVICE;
+typedef struct timeval *PTIMEVAL;
 
 int libbitfury_readHashData(unsigned int *res);
 int libbitfury_sendHashData(struct thr_info *thr, struct bitfury_device *bf, int chip_n);
