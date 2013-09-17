5a27b2ecb5ea1b76769f81d5486c25bd862aeb59
alpet83 <alpet1@gmail.com>
2013-09-17 00:40:08 -0700

    Added using screen and start logging

--- mine.sh
+++ mine.sh
@@ -1,4 +1,11 @@
 #!/bin/sh
 
-nice -19 ./cgminer --api-listen --api-port 4028 --api-network -c /home/pi/.cgminer/cgminer.conf 
+# cd /home/pi/test2/cgminer
+export DISPLAY=:0
+
+ts=`date +%F_%H-%M`
+echo $ts' Starting mine  ' >> /var/log/mining.log 
+
+screen -q -dmS "MoneyFab" /sbin/mine_start.sh
+# nice -19 /home/pi/test2/cgminer/cgminer --api-listen --api-port 4028 --api-network -c /home/pi/.cgminer/cgminer.conf 
  
