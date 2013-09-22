817b39d67d9a61f0921fa5a6d89cb4d53f901930
Alexander Petrov <alpet@hotmail.ru>
2013-09-22 18:02:09 +0400

    sync with needbmw fork

--- libbitfury.h
+++ libbitfury.h
@@ -27,17 +27,32 @@
 #ifndef __LIBBITFURY_H__
 #define __LIBBITFURY_H__
 
+#include "bitfury-config.h"
+
+#ifdef BITFURY_METABANK
+
+
+#define BITFURY_MAXCHIPS 256
+#define BITFURY_MAXBANKS 16
+#define BITFURY_BANKCHIPS 8
+#define BITFURY_SCANHASH_DELAY 60
+
+#endif
+
+#define BITFURY_ENABLE_SHORT_STAT 1
+#define BITFURY_ENABLE_LONG_STAT 1
+
+#ifdef BITFURY_NEEDBMW_NOMUX
+
+#define BITFURY_MAXCHIPS 100
+#define BITFURY_MAXBANKS 1
+#define BITFURY_BANKCHIPS 100
+#define BITFURY_SCANHASH_DELAY 100
+
+#endif
+
 #include "miner.h"
 
-#define BITFURY_STAT_N 1024
-#define BITFURY_MAXCHIPS 256
-#define BITFURY_MAXBANKS 1
-#define BITFURY_BANKCHIPS 60
-
-//#define BITFURY_ENABLE_SHORT_STAT 1
-//#define BITFURY_ENABLE_LONG_STAT 1
-
-#define BITFURY_API_STATS 300
 
 struct bitfury_payload {
 	unsigned char midstate[32];
@@ -50,18 +65,25 @@ struct bitfury_payload {
 
 struct bitfury_device {
 	unsigned osc6_bits;
+    unsigned osc6_bits_setpoint;
+    unsigned osc6_bits_upd;
 	unsigned newbuf[17];
 	unsigned oldbuf[17];
-	struct work * work;
-	struct work * owork;
-	struct work * o2work;
+    struct work *work;
+    struct work *owork;
+    struct work *o2work;
+    struct timeval work_start;
+    struct timeval work_end;
+    double work_median;  // median time in microseconds
+    double work_wait;    // средний простой в ожидании работы
 	int job_switched;
 	struct bitfury_payload payload;
 	struct bitfury_payload opayload;
 	struct bitfury_payload o2payload;
 	unsigned int results[16];
 	int results_n;
-	time_t stat_ts[BITFURY_STAT_N];
+    time_t stat_ts [BITFURY_STAT_N];
+    double stat_tsf[BITFURY_STAT_N];
 	unsigned int stat_counter;
 	unsigned int future_nonce;
 	unsigned int old_nonce;
@@ -81,15 +103,31 @@ struct bitfury_device {
 	double ns;
 	unsigned slot;
 	unsigned fasync;
-	unsigned strange_counter;
+	unsigned hw_errors;
+
+    int              alerts;
+    float            hw_rate;
+    bool             fixed_clk;
+    float            rbc_stat[4];   // rate-by-clock: 53, 54, 55, 56 associated to chip hash-rate
+    int              cch_stat[4];   // clock choice stat: сколько выбирался каждый клок по итогам соревнования
+    int              csw_back;      // clock switch back: сколько статистики прошло, после переключения частоты
+    int              csw_count;     // clock switch count: сколько раз сменялась частота принудительно
+    struct timeval   rst_time;      // reset time: когда устройство сбрасывалось последний раз
+
 	unsigned int matching_work;
+	unsigned int nonces[32];
+	int current_nonce;
+	double gh_stat[8];
 };
 
+typedef struct bitfury_device *PBITFURY_DEVICE;
+typedef struct timeval *PTIMEVAL;
+
 int libbitfury_readHashData(unsigned int *res);
-int libbitfury_sendHashData(struct bitfury_device *bf, int chip_n);
+int libbitfury_sendHashData(struct thr_info *thr, struct bitfury_device *bf, int chip_count);
 void work_to_payload(struct bitfury_payload *p, struct work *w);
 struct timespec t_diff(struct timespec start, struct timespec end);
 int libbitfury_detectChips(struct bitfury_device *devices);
-int libbitfury_shutdownChips(struct bitfury_device *devices, int chip_n);
+int libbitfury_shutdownChips(struct bitfury_device *devices, int chip_count);
 
 #endif /* __LIBBITFURY_H__ */
