9cd7668ea6afdecededb46bc85a13112845df96d
Alexander Petrov <alpet@hotmail.ru>
2013-09-23 18:58:46 +0400

    added killall cgminer (for avoid multiply instances runing)

--- mine_start.sh
+++ mine_start.sh
@@ -2,5 +2,6 @@
 cd /home/pi/test2/cgminer
 # export DISPLAY=:0
 # screen -q
+killall cgminer
 nice -19 /home/pi/test2/cgminer/cgminer --api-listen --api-port 4028 --api-network --compact -T -Q 10 -c /home/pi/.cgminer/cgminer.conf 
  
